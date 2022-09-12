e3e38cf572ea51ae842ec1cfd5d8d5b0735868b5
shaik <shaik@Farooq>
2022-09-12 21:05:36 +0530

    added testby classs

--- src/test/java/testing/TestBy.java
+++ src/test/java/testing/TestBy.java
@@ -1,5 +1,13 @@
 package testing;
 
-public class TestBy {
+import org.testng.annotations.Test;
 
+public class TestBy 
+{
+ @Test 
+ public void demo() 
+ {
+	 System.out.println("testing the test");
+ }
+ 
 }
