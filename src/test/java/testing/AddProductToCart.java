f7234327331ceb868ac949675bb33952a1365be6
shaik <shaik@Farooq>
2022-09-13 11:41:36 +0530

    Product added

---
+++ src/test/java/testing/AddProductToCart.java
@@ -0,0 +1,12 @@
+package testing;
+
+import org.testng.annotations.Test;
+
+public class AddProductToCart {
+@Test 
+public void addcartest() 
+{
+	System.out.println("product is added sucessfully");
+	System.out.println("product -2 added ");
+}
+}
