78e1dc34cefc34ee857d1991ff988502d18f9e88
shaik <shaik@Farooq>
2022-09-17 10:49:23 +0530

    Delete product by user-1

---
+++ src/test/java/testing/DeletePeoductFromCart.java
@@ -0,0 +1,10 @@
+package testing;
+
+import org.testng.annotations.Test;
+
+public class DeletePeoductFromCart {
+@Test
+public void deleteproduct() {
+	System.out.println("delete product sucessfully");
+}
+}
